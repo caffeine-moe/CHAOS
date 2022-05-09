3a1c26d69db2fc3d6f8dc55fe57fc83ac75e46f9
caffeine <juicyexe420@gmail.com>
2022-05-09 20:13:01 +0100

    fix le cat :DDDD

--- src/main/kotlin/org/caffeine/chaos/commands/Cat.kt
+++ src/main/kotlin/org/caffeine/chaos/commands/Cat.kt
@@ -10,6 +10,7 @@ import org.caffeine.chaos.api.client.message.MessageBuilder
 import org.caffeine.chaos.api.client.message.MessageCreateEvent
 import org.caffeine.chaos.api.json
 import org.caffeine.chaos.api.normalHTTPClient
+import org.caffeine.chaos.api.ua
 
 @kotlinx.serialization.Serializable
 private data class CatResponse(
@@ -18,11 +19,25 @@ private data class CatResponse(
 
 suspend fun cat(client: Client, event: MessageCreateEvent) = coroutineScope{
     if (event.message.content == "${client.config.prefix}cat" || event.message.content == "${client.config.prefix}meow"){
-        val response = json.decodeFromString<CatResponse>(normalHTTPClient.get("http://aws.random.cat/meow").bodyAsText())
+        val response = normalHTTPClient.get("http://aws.random.cat/meow"){
+            headers {
+                append("Host","aws.random.cat")
+                append("Connection", "keep-alive")
+                append("Pragma", "no-cache")
+                append("Cache-Control", "no-cache")
+                append("DNT", "1")
+                append("Upgrade-Insecure-Requests", "1")
+                append("User-Agent", ua)
+                append("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                append("Sec-GPC", "1")
+                append("Upgrade-Insecure-Requests", "1")
+            }
+        }
+        val cat = json.decodeFromString<CatResponse>(response.bodyAsText())
         event.channel.sendMessage(
             MessageBuilder()
                 .appendLine("**Meow!!**")
-                .appendLine(response.file)
+                .appendLine(cat.file)
                 .build(), client
         ).thenAccept { message ->
                 this.launch { onComplete(message, client, client.config.auto_delete.bot.content_generation) }
