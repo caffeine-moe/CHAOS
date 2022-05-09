3119615b9726fd50985e1771911858e587ac9f85
caffeine <juicyexe420@gmail.com>
2022-05-09 19:55:20 +0100

    fix dumb nitro sniper

--- src/main/kotlin/org/caffeine/chaos/NitroSniper.kt
+++ src/main/kotlin/org/caffeine/chaos/NitroSniper.kt
@@ -6,23 +6,18 @@ import org.caffeine.chaos.api.client.ClientUserRedeemedCodeStatus
 import org.caffeine.chaos.api.client.message.MessageCreateEvent
 
 suspend fun nitroSniper(event: MessageCreateEvent, client: Client) {
-    val giftPrefix = "discord.gift/"
-    val rg = giftPrefix + ".{16,24}".toRegex()
-    val match = rg.toRegex().matches(event.message.content)
-    if (match) {
-        val code = event.message.content.removePrefix(giftPrefix)
+    val rg = ("discord.gift/" + ".{16,24}".toRegex()).toRegex()
+    if (rg.matches(event.message.content)) {
+        val code = event.message.content.removePrefix("discord.gift/")
         client.user.redeemCode(code).thenAccept { rc ->
-            if (!client.config.logger.nitro_sniper) {
+            if (client.config.logger.nitro_sniper) {
                 when (rc.status) {
                     ClientUserRedeemedCodeStatus.SUCCESS -> {
                         log("Redeemed code ${rc.code}! in ${rc.latency}ms", "NITRO SNIPER:")
                     }
                     ClientUserRedeemedCodeStatus.INVALID -> {
-                        when (rc.error) {
-                            ClientUserRedeemedCodeError.UNKNOWN_CODE -> {
-                                log("Code ${rc.code} was invalid! (${rc.latency}ms)", "NITRO SNIPER:")
-                            }
-                            else -> {}
+                        if (rc.error == ClientUserRedeemedCodeError.UNKNOWN_CODE) {
+                            log("Code ${rc.code} was invalid! (${rc.latency}ms)", "NITRO SNIPER:")
                         }
                     }
                 }
