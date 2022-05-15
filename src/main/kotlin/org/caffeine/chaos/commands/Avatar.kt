a61b0287b238b35b4a7b3d6e2a49ad6eff7032b7
sexnine <25860388+sexnine@users.noreply.github.com>
2022-05-16 06:11:50 +1000

    Refactor Avatar command (sexy)

--- src/main/kotlin/org/caffeine/chaos/commands/Avatar.kt
+++ src/main/kotlin/org/caffeine/chaos/commands/Avatar.kt
@@ -4,35 +4,44 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import org.caffeine.chaos.Command
 import org.caffeine.chaos.api.client.Client
+import org.caffeine.chaos.api.client.message.Message
 import org.caffeine.chaos.api.client.message.MessageBuilder
 import org.caffeine.chaos.api.client.message.MessageCreateEvent
 
 class Avatar : Command(arrayOf("av", "avatar")) {
-    override suspend fun onCalled(client: Client, event: MessageCreateEvent, args: MutableList<String>) = coroutineScope{
-        if (args.isEmpty()){
-            event.channel.sendMessage(MessageBuilder()
-                .appendLine("${client.user.discriminatedName}'s Avatar")
-                .appendLine(client.user.avatarUrl())
-                .build())
-                .thenAccept { this.launch { onComplete(it, client, client.config.auto_delete.bot.content_generation) } }
-            return@coroutineScope
-        }
-        if (event.message.mentions.isNotEmpty()){
-            event.channel.sendMessage(MessageBuilder()
-                .appendLine("${event.message.mentions.first().discriminatedName}'s Avatar")
-                .appendLine(event.message.mentions.first().avatarUrl())
-                .build())
-                .thenAccept { this.launch { onComplete(it, client, client.config.auto_delete.bot.content_generation) } }
-        }
-        if (args.isNotEmpty() && event.message.mentions.isEmpty()) {
+    override suspend fun onCalled(client: Client, event: MessageCreateEvent, args: MutableList<String>) =
+        coroutineScope {
+            if (args.isNotEmpty() && event.message.mentions.isEmpty()) {
+                event.channel.sendMessage(
+                    MessageBuilder()
+                        .appendLine("Incorrect usage '${event.message.content}'")
+                        .appendLine("Error: '${args.joinToString(" ")}}' is not a mentioned user.")
+                        .appendLine("Correct usage: `${client.config.prefix}info @user`")
+                        .build()
+                ).thenAccept { message ->
+                    this.launch { onComplete(message, client, true) }
+                }
+                return@coroutineScope
+            }
+
+            val userName: String
+            val avatarURL: String
+
+            if (args.isEmpty()) {
+                userName = client.user.discriminatedName;
+                avatarURL = client.user.avatarUrl()
+            } else {
+                val user = event.message.mentions.first();
+                userName = user.discriminatedName;
+                avatarURL = user.avatarUrl()
+            }
+
             event.channel.sendMessage(
                 MessageBuilder()
-                    .appendLine("Incorrect usage '${event.message.content}'")
-                    .appendLine("Error: '${args.joinToString(" ")}}' is not a mentioned user.")
-                    .appendLine("Correct usage: `${client.config.prefix}info @user`")
-                    .build()).thenAccept { message ->
-                this.launch { onComplete(message, client, true) }
-            }
+                    .appendLine("${userName}'s Avatar")
+                    .appendLine(avatarURL)
+                    .build()
+            )
+                .thenAccept { this.launch { onComplete(it, client, client.config.auto_delete.bot.content_generation) } }
         }
-    }
 }
