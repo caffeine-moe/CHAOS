c810a096b5e1f80bdb1b41fee69b41a877b3e723
caffeine <caffeine@caffeine.moe>
2022-05-20 23:08:20 +0100

    make antiscam unable to block a user more than once LOL

--- src/main/kotlin/org/caffeine/chaos/api/client/message/MessageAuthor.kt
+++ src/main/kotlin/org/caffeine/chaos/api/client/message/MessageAuthor.kt
@@ -1,6 +1,8 @@
 package org.caffeine.chaos.api.client.message
 
+import org.caffeine.chaos.api.client.Client
 import org.caffeine.chaos.api.client.DiscordUser
+import org.caffeine.chaos.api.discordHTTPClient
 
 @kotlinx.serialization.Serializable
 data class MessageAuthor(
@@ -10,4 +12,11 @@ data class MessageAuthor(
     override val avatar: String? = "",
 ) : DiscordUser(username, discriminator, id, avatar) {
     val discriminatedName = "$username#$discriminator"
+    suspend fun blocked(client: Client) : Boolean {
+        var torf = false
+        if (client.user.relationships.blockedUsers.getList().contains(this)) {
+            torf = true
+        }
+        return torf
+    }
 }
