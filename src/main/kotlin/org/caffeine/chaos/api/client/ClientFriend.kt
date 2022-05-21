9727b8136bdb7c039dfa8b929d6177c94c090bdd
caffeine <caffeine@caffeine.moe>
2022-05-21 14:53:36 +0100

    remove duplicate functions

--- src/main/kotlin/org/caffeine/chaos/api/client/ClientFriend.kt
+++ src/main/kotlin/org/caffeine/chaos/api/client/ClientFriend.kt
@@ -1,58 +1,9 @@
 package org.caffeine.chaos.api.client
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import kotlinx.serialization.decodeFromString
-import org.caffeine.chaos.api.BASE_URL
-import org.caffeine.chaos.api.discordHTTPClient
-import org.caffeine.chaos.api.jsonc
-import org.caffeine.chaos.api.token
-
 @kotlinx.serialization.Serializable
 data class ClientFriend(
     override val username: String,
     override val discriminator: String,
     override val id: String,
     override val avatar: String?,
-) : DiscordUser(username, discriminator, id, avatar) {
-    val discriminatedName = "$username#$discriminator"
-    fun avatarUrl(): String {
-        var av = "null"
-        if (!avatar.isNullOrBlank()) {
-            av = "https://cdn.discordapp.com/avatars/$id/$avatar?size=4096"
-            if (avatar.startsWith("a_")) {
-                av = "https://cdn.discordapp.com/avatars/$id/$avatar.gif?size=4096"
-            }
-        }
-        return av
-    }
-
-    suspend fun userInfo(): DiscordUserInfo {
-        val re = discordHTTPClient.get("$BASE_URL/users/$id") {
-            headers {
-                append(HttpHeaders.Authorization, token)
-            }
-        }
-        val de = jsonc.decodeFromString<DiscordUserInfo>(re.bodyAsText())
-        var ba = "null"
-        if (!de.banner.isNullOrBlank()) {
-            ba = "https://cdn.discordapp.com/banners/${id}/${de.banner}?size=1024"
-            if (de.banner.startsWith("a_")) {
-                ba = "https://cdn.discordapp.com/banners/${id}/${de.banner}.gif?size=1024"
-            }
-        }
-        return DiscordUserInfo(
-            de.accentColor,
-            avatarUrl(),
-            de.avatarDecoration,
-            ba,
-            de.bannerColor,
-            de.bot,
-            de.discriminator,
-            de.id,
-            de.publicFlags,
-            de.username
-        )
-    }
-}
+) : DiscordUser(username, discriminator, id, avatar)
