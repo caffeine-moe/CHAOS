c1e326f0af53f6016f0d225817318245d95f9df9
caffeine <caffeine@caffeine.moe>
2022-05-21 17:48:32 +0100

    fix antiscam

--- src/main/kotlin/org/caffeine/chaos/AntiScam.kt
+++ src/main/kotlin/org/caffeine/chaos/AntiScam.kt
@@ -3,6 +3,7 @@ package org.caffeine.chaos
 import org.caffeine.chaos.api.client.Client
 import org.caffeine.chaos.api.client.message.MessageCreateEvent
 import org.caffeine.chaos.api.scamLinks
+import java.net.URL
 import kotlin.math.absoluteValue
 
 @kotlinx.serialization.Serializable
@@ -11,26 +12,26 @@ data class AntiScamResponse(
 )
 
 suspend fun antiScam(client: Client, event: MessageCreateEvent) {
-    if (event.message.author.id != client.user.id) {
-        val start = System.currentTimeMillis()
-        var scamLink = ""
-        val scam = scamLinks.any { link -> scamLink = link; event.message.content.contains(link) }
-        if (scam) {
-            if (client.config.logger.anti_scam) {
-                log("Found scam link \"${scamLink}\" in channel ${event.channel.id} by ${event.message.author.discriminatedName} in ${(System.currentTimeMillis() - start).absoluteValue}ms.",
-                    "ANTI SCAM:")
-                if (client.config.anti_scam.block && !event.message.author.isBlocked(client)) {
-                    client.user.block(event.message.author.id)
-                    log("Blocked user ${event.message.author.discriminatedName} in ${(System.currentTimeMillis() - start).absoluteValue}ms.",
-                        "ANTI SCAM:")
-                    return
-                }
-                return
-            }
-            if (client.config.anti_scam.block) {
-                client.user.block(event.message.author.id)
-                return
-            }
+    if (event.message.author.id == client.user.id) return
+    val start = System.currentTimeMillis()
+    val url = event.message.content.split(" ").find { link -> link.matches("http://.*..*|https://.*..*".toRegex()) }
+        ?: return
+    val scam = scamLinks.contains(URL(url).host)
+    if (!scam) return
+    val time = (System.currentTimeMillis() - start).absoluteValue
+    if (client.config.logger.anti_scam) {
+        log("Found scam link \"${url}\" in channel ${event.channel.id} by ${event.message.author.discriminatedName} in ${time}ms.",
+            "ANTI SCAM:")
+        if (client.config.anti_scam.block && !event.message.author.isBlocked(client)) {
+            client.user.block(event.message.author.id)
+            log("Blocked user ${event.message.author.discriminatedName} in ${time}ms.",
+                "ANTI SCAM:")
+            return
         }
+        return
+    }
+    if (client.config.anti_scam.block) {
+        client.user.block(event.message.author.id)
+        return
     }
 }
