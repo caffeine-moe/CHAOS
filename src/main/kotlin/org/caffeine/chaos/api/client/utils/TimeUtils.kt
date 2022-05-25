ca4e2db004e3094cbfeeb331e14c2833e6dafe5d
caffeine <caffeine@caffeine.moe>
2022-05-25 17:18:02 +0100

    remove class from TimeUtils.kt

--- src/main/kotlin/org/caffeine/chaos/api/client/utils/TimeUtils.kt
+++ src/main/kotlin/org/caffeine/chaos/api/client/utils/TimeUtils.kt
@@ -2,13 +2,11 @@ package org.caffeine.chaos.api.client.utils
 
 import kotlin.math.absoluteValue
 
-class TimeUtils {
-    fun calcNonce(): String {
-        val unixTs = System.currentTimeMillis()
-        return ((unixTs - 1420070400000) * 4194304).absoluteValue.toString()
-    }
+fun calcNonce(): String {
+    val unixTs = System.currentTimeMillis()
+    return ((unixTs - 1420070400000) * 4194304).absoluteValue.toString()
+}
 
-    fun convertIdToUnix(id: String): Long {
-        return (id.toLong() / 4194304 + 1420070400000).absoluteValue
-    }
+fun convertIdToUnix(id: String): Long {
+    return (id.toLong() / 4194304 + 1420070400000).absoluteValue
 }
