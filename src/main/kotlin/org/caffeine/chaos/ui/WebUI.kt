f766d78319ac60f747847adf23f3a362f653f639
caffeine <caffeine@caffeine.moe>
2022-05-28 20:06:05 +0100

    intellij idea auto clean lol

--- src/main/kotlin/org/caffeine/chaos/ui/WebUI.kt
+++ src/main/kotlin/org/caffeine/chaos/ui/WebUI.kt
@@ -5,6 +5,8 @@ import org.caffeine.chaos.api.client.Client
 
 class WebUI {
     suspend fun init(client: Client) = coroutineScope {
-        if (!client.config.web_ui.enabled) { return@coroutineScope }
+        if (!client.config.web_ui.enabled) {
+            return@coroutineScope
+        }
     }
 }
