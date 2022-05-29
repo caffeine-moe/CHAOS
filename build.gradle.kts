20dfa0726f87fc5c26e163ede58ae921ea050547
caffeine <caffeine@caffeine.moe>
2022-05-29 19:25:12 +0100

    update ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,9 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     implementation("commons-validator:commons-validator:1.7")
-    implementation("io.ktor:ktor-client-core:2.0.1")
-    implementation("io.ktor:ktor-client-cio:2.0.1")
-    implementation("io.ktor:ktor-client-content-negotiation:2.0.1")
+    implementation("io.ktor:ktor-client-core:2.0.2")
+    implementation("io.ktor:ktor-client-cio:2.0.2")
+    implementation("io.ktor:ktor-client-content-negotiation:2.0.2")
     implementation("com.github.lalyos:jfiglet:0.0.9")
     implementation("com.github.oshi:oshi-core:6.1.6")
 }
