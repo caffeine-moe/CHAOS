b2ce1109a6f8555d817994808d95ad9f174ef927
caffeine <caffeine@caffeine.moe>
2022-05-30 13:49:38 +0100

    make things non nullable

--- src/main/kotlin/org/caffeine/chaos/api/client/message/Message.kt
+++ src/main/kotlin/org/caffeine/chaos/api/client/message/Message.kt
@@ -8,16 +8,16 @@ import java.util.concurrent.CompletableFuture
 
 @kotlinx.serialization.Serializable
 class Message(
-    val id: String?,
+    val id: String = "",
     val content: String = "",
     val channel_id: String = "",
     val author: MessageAuthor = MessageAuthor("", "", "", ""),
     val attachments: List<MessageAttachment> = mutableListOf(),
-    val embeds: List<MessageEmbed>? = null,
-    val mention_everyone: Boolean? = false,
-    val mention_roles: List<String>? = null,
+    val embeds: List<MessageEmbed> = mutableListOf(),
+    val mention_everyone: Boolean = false,
+    val mention_roles: List<String> = mutableListOf(),
     val mentions: List<MessageMention> = mutableListOf(),
-    val pinned: Boolean? = false,
+    val pinned: Boolean = false,
     val referenced_message: Message? = null,
     val type: Int = 0,
 ) {
