985b3b2f56c0fb4c6c4d45cf3afaa3204bcb17cc
caffeine <caffeine@caffeine.moe>
2022-05-30 16:46:21 +0100

    allow closing group dms

--- src/main/kotlin/org/caffeine/chaos/commands/CloseDm.kt
+++ src/main/kotlin/org/caffeine/chaos/commands/CloseDm.kt
@@ -7,10 +7,12 @@ import org.caffeine.chaos.log
 
 class CloseDm : Command(arrayOf("closedm")) {
     override suspend fun onCalled(client: Client, event: MessageCreateEvent, args: MutableList<String>, cmd: String) {
-        if (event.channel.type() == 1) {
-            event.channel.delete()
-            log("Channel ${event.channel.id} closed!", "RESPONSE:")
-            return
+        when (event.channel.type()) {
+            1, 3 -> {
+                event.channel.delete()
+                log("Channel ${event.channel.id} closed!", "RESPONSE:")
+                return
+            }
         }
         log("Channel ${event.channel.id} is not a DM Channel!", "RESPONSE:")
     }
