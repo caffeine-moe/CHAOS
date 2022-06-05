ee3d4cdbb64b55b000747e7f8811072cdcbe3b91
caffeine <caffeine@caffeine.moe>
2022-06-05 19:09:14 +0100

    lollo

--- src/main/kotlin/org/caffeine/chaos/api/SendJsonRequest.kt
+++ src/main/kotlin/org/caffeine/chaos/api/SendJsonRequest.kt
@@ -2,22 +2,13 @@ package org.caffeine.chaos.api
 
 import io.ktor.websocket.*
 import org.caffeine.chaos.log
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import kotlin.coroutines.cancellation.CancellationException
 
 suspend fun sendJsonRequest(connection : Connection, request : String) {
     try {
         connection.ws.send(request)
-    } catch (e : Exception) {
-        if (e is CancellationException) {
-            log("Websocket disconnected, reconnecting...", "API:")
-            connection.reconnect()
-        }
-        println(e)
-        e.printStackTrace()
-        println(e.cause)
-        println("sendjsonrequest")
-        println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yy hh:mm:ss")))
+    } catch (e : CancellationException) {
+        log("Websocket disconnected, reconnecting...", "API:")
+        connection.reconnect()
     }
 }
