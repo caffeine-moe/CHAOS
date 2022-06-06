8f69d79a43303f1202e855e48d8f0f82099853b8
caffeine <caffeine@caffeine.moe>
2022-06-06 17:43:59 +0100

    extra alias

--- src/main/kotlin/org/caffeine/chaos/commands/IdToDate.kt
+++ src/main/kotlin/org/caffeine/chaos/commands/IdToDate.kt
@@ -9,7 +9,7 @@ import org.caffeine.chaos.api.client.message.MessageBuilder
 import org.caffeine.chaos.api.client.message.MessageCreateEvent
 import org.caffeine.chaos.api.client.utils.convertIdToUnix
 
-class IdToDate : Command(arrayOf("idtodate", "idtd"),
+class IdToDate : Command(arrayOf("idtodate", "idtod", "idtd"),
     CommandInfo("IdToDate", "idtodate <Discord ID>", "Converts any discord id to a normal date format.")) {
     override suspend fun onCalled(
         client : Client,
