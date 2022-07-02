79be1b412eaa19cd4c8f3343fda822d40cd6b8bb
caffeine <caffeine@caffeine.moe>
2022-07-02 10:29:15 +0100

    add comments

--- src/main/kotlin/org/caffeine/chaos/ConfigWatcher.kt
+++ src/main/kotlin/org/caffeine/chaos/ConfigWatcher.kt
@@ -9,28 +9,27 @@ import java.nio.file.StandardWatchEventKinds
 
 //checks if anything happens to the config file, if something does, then reload
 suspend fun configWatcher(client : Client) {
+    //create a watch service
     val watchService = withContext(Dispatchers.IO) {
         FileSystems.getDefault().newWatchService()
     }
+    //create a watch key for the root directory
     val path = Paths.get("").toAbsolutePath()
     val watchKey = withContext(Dispatchers.IO) {
         path.register(
             watchService,
-            StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE
+            StandardWatchEventKinds.ENTRY_MODIFY
         )
     }
+    //loop forever
     while (true) {
+        //wait for an event
         for (event in watchKey.pollEvents()) {
-            val kind = event.kind()
-            if (kind == StandardWatchEventKinds.ENTRY_DELETE && event.context().toString()
+            //if the event is a modification to the config file
+            if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY && event.context().toString()
                     .contains("config.json")
             ) {
-                client.logout()
-                main()
-            }
-            if (kind == StandardWatchEventKinds.ENTRY_MODIFY && event.context().toString()
-                    .contains("config.json")
-            ) {
+                //reload the config
                 client.logout()
                 main()
             }
