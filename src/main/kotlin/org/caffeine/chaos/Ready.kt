4e0d3ff1be171880d865eda0d18fa63c53f93a20
caffeine <caffeine@caffeine.moe>
2022-10-03 17:29:01 +0100

    remove unneeded coroutine

--- src/main/kotlin/org/caffeine/chaos/Ready.kt
+++ src/main/kotlin/org/caffeine/chaos/Ready.kt
@@ -3,16 +3,18 @@ package org.caffeine.chaos
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import org.caffeine.chaos.api.client.Client
+import org.caffeine.chaos.api.models.interfaces.TextBasedChannel
+import org.caffeine.chaos.api.models.message.MessageFilters
+import org.caffeine.chaos.api.models.message.MessageSearchFilters
 import org.caffeine.chaos.api.utils.ConsoleColours
+import org.caffeine.chaos.api.utils.MessageBuilder
 import org.caffeine.chaos.api.utils.clear
 import org.caffeine.chaos.api.utils.log
 
-suspend fun ready(client : Client) = coroutineScope {
-    launch {
-        log("${ConsoleColours.BLUE.value}Welcome to CHAOS!")
-        clear()
-        registerCommands()
-        loginPrompt(client)
-        configWatcher(client)
-    }
+suspend fun ready(client : Client) {
+    log("${ConsoleColours.BLUE.value}Welcome to CHAOS!")
+    clear()
+    registerCommands()
+    loginPrompt(client)
+    configWatcher(client)
 }
