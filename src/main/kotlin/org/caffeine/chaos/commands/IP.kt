49600b882840b8d46973125bf3f85a83068b1bbd
caffeine <caffeine@caffeine.moe>
2022-12-03 18:05:16 +0000

    fix ip lookup

--- src/main/kotlin/org/caffeine/chaos/commands/IP.kt
+++ src/main/kotlin/org/caffeine/chaos/commands/IP.kt
@@ -7,7 +7,9 @@ import io.ktor.network.sockets.*
 import io.ktor.util.network.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.decodeFromString
 import org.caffeine.chaos.Command
 import org.caffeine.chaos.CommandInfo
@@ -20,74 +22,31 @@ import java.net.InetAddress
 import java.net.URL
 
 class IP : Command(arrayOf("ip"), CommandInfo("IP", "ip <IP/URL>", "Looks up information about a specified IP/URL.")) {
+
     @Serializable
-    private data class IpApiResponse(
-        val borders : String = "",
-        val callingCode : String = "",
-        val capital : String = "",
+    data class IPAPIResponse(
+        @SerialName("as")
+        val asn : String = "",
         val city : String = "",
-        val connection : Connection = Connection(),
         val continent : String = "",
         val continentCode : String = "",
         val country : String = "",
         val countryCode : String = "",
-        val currency : Currency = Currency(),
-        val flag : Flag = Flag(),
-        val ip : String = "",
-        val isEu : Boolean = false,
-        val latitude : Double = 0.0,
-        val longitude : Double = 0.0,
-        val postal : String = "",
-        val region : String = "",
-        val regionCode : String = "",
-        val security : Security = Security(),
-        val success : Boolean = false,
-        val message : String = "",
-        val timezone : Timezone = Timezone(),
-        val type : String = "",
-    )
-
-    @Serializable
-    private data class Connection(
-        val asn : Int = 0,
-        val domain : String = "",
-        val isp : String = "",
-        val org : String = "",
-    )
-
-    @Serializable
-    private data class Currency(
-        val code : String = "",
-        val exchangeRate : Int = 0,
-        val name : String = "",
-        val plural : String = "",
-        val symbol : String = "",
-    )
-
-    @Serializable
-    private data class Flag(
-        val emoji : String = "",
-        val emojiUnicode : String = "",
-        val img : String = "",
-    )
-
-    @Serializable
-    private data class Security(
-        val anonymous : Boolean = false,
+        val currency : String = "",
         val hosting : Boolean = false,
+        val isp : String = "",
+        val lat : Double = 0.0,
+        val lon : Double = 0.0,
+        val mobile : Boolean = false,
+        val org : String = "",
         val proxy : Boolean = false,
-        val tor : Boolean = false,
-        val vpn : Boolean = false,
-    )
-
-    @Serializable
-    private data class Timezone(
-        val abbr : String = "",
-        val currentTime : String = "",
-        val id : String = "",
-        val isDst : Boolean = false,
-        val offset : Int = 0,
-        val utc : String = "",
+        val region : String = "",
+        val regionName : String = "",
+        val status : String = "",
+        val timezone : String = "",
+        val zip : String = "",
+        @Transient
+        val message : String = "",
     )
 
     override suspend fun onCalled(
@@ -118,30 +77,24 @@ class IP : Command(arrayOf("ip"), CommandInfo("IP", "ip <IP/URL>", "Looks up inf
                         InetAddress.getByName(host).hostAddress
                     }
                     val response =
-                        normalHTTPClient.request("https://ipwhois.pro/$ip?key=Sxd2AkU2ZL0YtkSR&security=1&lang=en") {
-                            headers {
-                                append("Referer", "https://ipwhois.io/")
-                            }
-                        }
+                        normalHTTPClient.request("http://ip-api.com/json/$ip?fields=28561407")
                     val parsedResponse =
-                        json.decodeFromString<IpApiResponse>(response.bodyAsText())
-                    when (parsedResponse.success) {
+                        json.decodeFromString<IPAPIResponse>(response.bodyAsText())
+                    when (parsedResponse.status == "success") {
                         true -> {
                             message.edit(
                                 MessageBuilder()
                                     .appendLine("**Information for IP/URL $url**")
-                                    .appendLine("**IP:** ${parsedResponse.ip}")
+                                    .appendLine("**IP:** $ip")
                                     .appendLine("**Continent:** ${parsedResponse.continent}")
                                     .appendLine("**Country:** ${parsedResponse.country}")
                                     .appendLine("**Region:** ${parsedResponse.region}")
                                     .appendLine("**City:** ${parsedResponse.city}")
-                                    .appendLine("**Zip/Postal:** ${parsedResponse.postal}")
-                                    .appendLine("**Timezone:** ${parsedResponse.timezone.id}")
-                                    .appendLine("**ISP:** ${parsedResponse.connection.isp}")
-                                    .appendLine("**Proxy:** ${parsedResponse.security.proxy}")
-                                    .appendLine("**VPN:** ${parsedResponse.security.vpn}")
-                                    .appendLine("**Hosting:** ${parsedResponse.security.hosting}")
-                                    .appendLine("**Tor:** ${parsedResponse.security.tor}")
+                                    .appendLine("**Zip/Postal:** ${parsedResponse.zip}")
+                                    .appendLine("**Timezone:** ${parsedResponse.timezone}")
+                                    .appendLine("**ISP:** ${parsedResponse.isp}")
+                                    .appendLine("**Proxy:** ${parsedResponse.proxy}")
+                                    .appendLine("**Hosting:** ${parsedResponse.hosting}")
                             )
                                 .await().also { message -> onComplete(message, true) }
                         }
