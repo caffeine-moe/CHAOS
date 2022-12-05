cf44c8e88de76cc217ed697c873361b5b29b31c9
caffeine <caffeine@caffeine.moe>
2022-12-05 07:19:57 +0000

    extra config stuff

--- src/main/kotlin/org/caffeine/chaos/Main.kt
+++ src/main/kotlin/org/caffeine/chaos/Main.kt
@@ -75,17 +75,22 @@ suspend fun main(args : Array<String> = arrayOf()) = coroutineScope {
 
 suspend fun loadConfig() = coroutineScope {
 
+    val prefix = "ERROR:"
+
     if (!configFile.exists()) {
-        val default = javaClass.classLoader.getResource("defaultconfig.json")
+        val default = javaClass.classLoader.getResource("defaultconfig.json") ?: run { log(
+            "Config not found, and we were unable to generate one for you, please create a config file and put this in it https://caffeine.moe/CHAOS/config.json",
+            prefix
+        ); return@coroutineScope }
         this.also {
             configFile.createNewFile()
         }
-        configFile.writeText(default!!.readText())
+        configFile.writeText(default.readText())
         log(
             "Config not found, we have generated one for you at ${configFile.absolutePath}",
-            "${ConsoleColour.RED.value}ERROR:"
+            prefix
         )
-        log("${ConsoleColour.BLUE.value}Please change the file accordingly. Documentation: https://caffeine.moe/CHAOS/")
+        log("Please change the file accordingly. Documentation: https://caffeine.moe/CHAOS/")
         exitProcess(0)
     }
     try {
@@ -93,7 +98,7 @@ suspend fun loadConfig() = coroutineScope {
     } catch (e : SerializationException) {
         log(
             "Unable to interpret config, please make sure that the one you have is structured the same as the one here: https://caffeine.moe/CHAOS/config.json",
-            "ERROR:",
+            prefix
         )
         log("Full stacktrace here:")
         e.printStackTrace()
