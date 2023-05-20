875c44ab5af38f704e880ef5c71b180a5977aa45
caffeine <caffeine@caffeine.moe>
2023-05-20 21:00:14 +0100

    fix another oopsie

--- src/main/kotlin/org/caffeine/chaos/api/client/connection/payloads/client/DMCreateRequest.kt
+++ src/main/kotlin/org/caffeine/chaos/api/client/connection/payloads/client/DMCreateRequest.kt
@@ -2,9 +2,10 @@ package org.caffeine.chaos.api.client.connection.payloads.client
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.caffeine.chaos.api.entities.Snowflake
 
 @Serializable
 data class DMCreateRequest(
     @SerialName("recipient_id")
-    val userId : String,
+    val userId : Snowflake,
 )
