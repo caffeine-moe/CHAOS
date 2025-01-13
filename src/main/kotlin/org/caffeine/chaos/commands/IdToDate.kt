e1c7c40af97bdb21ab46f4597609c8cad85354a3
caffeine01 <caffeine@caffeine.moe>
2025-01-13 21:33:11 +0000

    command stuff

--- src/main/kotlin/org/caffeine/chaos/commands/IdToDate.kt
+++ src/main/kotlin/org/caffeine/chaos/commands/IdToDate.kt
@@ -11,37 +11,37 @@ class IdToDate : Command(
     CommandInfo("IdToDate", "idtodate <Discord ID>", "Converts any discord id to a normal date format.")
 ) {
     override suspend fun onCalled(
-        client : Client,
-        event : ClientEvent.MessageCreate,
-        args : List<String>,
-        cmd : String,
+        client: Client,
+        event: ClientEvent.MessageCreate,
+        args: List<String>,
+        cmd: String,
     ) {
-        var id : Long = 0
+        var id: Long = 0
         val err = if (args.isEmpty()) {
             "Not enough arguments."
         } else {
             try {
                 id = args.first().toLong()
                 ""
-            } catch (_ : NumberFormatException) {
+            } catch (_: NumberFormatException) {
                 "'${args.joinToString(" ")}' is not a discord id."
             }
         }
-        if (err.isEmpty()) {
-            val simpleDateFormat = java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-            val date = java.util.Date(id.toString().asSnowflake().timestamp.toEpochMilliseconds())
-            val formattedDate = simpleDateFormat.format(date)
+        if (err.isNotEmpty()) {
             event.channel.sendMessage(
-                MessageBuilder()
-                    .appendLine("**The id $id as a date is:**")
-                    .appendLine(formattedDate)
+                error(client, event, err, commandInfo)
             ).awaitThen {
                 onComplete(it, true)
             }
             return
         }
+        val simpleDateFormat = java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val date = java.util.Date(id.toString().asSnowflake().timestamp.toEpochMilliseconds())
+        val formattedDate = simpleDateFormat.format(date)
         event.channel.sendMessage(
-            error(client, event, err, commandInfo)
+            MessageBuilder()
+                .appendLine("**The id $id as a date is:**")
+                .appendLine(formattedDate)
         ).awaitThen {
             onComplete(it, true)
         }
